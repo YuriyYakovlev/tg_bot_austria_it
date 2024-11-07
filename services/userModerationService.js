8e73be35305d7b5ba2d9cca1a68bb9bb547b8734
Yuriy Yakovlev <yakovlev.yuriy@gmail.com>
2024-11-07 09:28:53 +0100

    no need in notifications

--- services/userModerationService.js
+++ services/userModerationService.js
@@ -1,8 +1,8 @@
 // userModerationService.js
 const db = require("../db/connectors/dbConnector");
 const config = require("../config/config");
-const chatSettingsService = require('./chatSettingsService');
-const languageService = require('./languageService');
+// const chatSettingsService = require('./chatSettingsService');
+// const languageService = require('./languageService');
 
 async function identifyAndMarkSpammers() {
   try {
@@ -58,7 +58,7 @@ async function kickUserIfNotAdmin(bot, chatId, userId) {
 }
 
 async function kickSpammers(bot) {
-  const spammersPerChat = new Map();
+  // const spammersPerChat = new Map();
 
   try {
     const spammers = await identifyAndMarkSpammers();
@@ -67,53 +67,53 @@ async function kickSpammers(bot) {
         // Attempt to kick the user
         const wasKicked = await kickUserIfNotAdmin(bot, spammer.chatId, spammer.userId);
 
-        if (wasKicked) {
-          if (spammersPerChat.has(spammer.chatId)) {
-            spammersPerChat.set(spammer.chatId, spammersPerChat.get(spammer.chatId) + 1);
-          } else {
-            spammersPerChat.set(spammer.chatId, 1);
-          }
-        }
+        // if (wasKicked) {
+        //   if (spammersPerChat.has(spammer.chatId)) {
+        //     spammersPerChat.set(spammer.chatId, spammersPerChat.get(spammer.chatId) + 1);
+        //   } else {
+        //     spammersPerChat.set(spammer.chatId, 1);
+        //   }
+        // }
       } catch (error) {
-        console.error(`Failed to kick spammer with userId: ${spammer.userId}`, error.message);
+        console.error(`Failed to kick user: ${spammer.userId}`, error.message);
       }
     }
 
-    for (const [chatId, count] of spammersPerChat.entries()) {
-      try {
-        const language = await chatSettingsService.getLanguageForChat(chatId);
-        const messages = languageService.getMessages(language).messages;
-        const notificationMessage = messages.banSpammersComplete(count);
+    // for (const [chatId, count] of spammersPerChat.entries()) {
+    //   try {
+    //     const language = await chatSettingsService.getLanguageForChat(chatId);
+    //     const messages = languageService.getMessages(language).messages;
+    //     const notificationMessage = messages.banSpammersComplete(count);
         
-        sendTemporaryMessage(bot, chatId, notificationMessage, config.KICKED_MESSAGE_DURATION_SEC * 1000, {
-          disable_notification: true
-        });
-        console.log(`Sent notification to chat: ${chatId}. Kicked users: ${count} `);
-      } catch (error) {
-        console.error(`Failed to send kicked users notification to chatId: ${chatId}`, error.message);
-      }
-    }
+    //     sendTemporaryMessage(bot, chatId, notificationMessage, config.KICKED_MESSAGE_DURATION_SEC * 1000, {
+    //       disable_notification: true
+    //     });
+    //     console.log(`Sent notification to chat: ${chatId}. Kicked users: ${count} `);
+    //   } catch (error) {
+    //     console.error(`Failed to send kicked users notification to chatId: ${chatId}`, error.message);
+    //   }
+    // }
   } catch (error) {
     console.error("Failed to kick users:", error.message);
   }
 }
 
-async function sendTemporaryMessage(bot, chatId, message, timeoutMs, options = null) {
-  if(!chatId){
-    console.log(`sendTemporatyMessage: chatId is null. message: ${message}`);
-    return;
-  }
-  try {
-      const sentMessage = await bot.sendMessage(chatId, message, options);
-      const messageId = sentMessage.message_id;
+// async function sendTemporaryMessage(bot, chatId, message, timeoutMs, options = null) {
+//   if(!chatId){
+//     console.log(`sendTemporatyMessage: chatId is null. message: ${message}`);
+//     return;
+//   }
+//   try {
+//       const sentMessage = await bot.sendMessage(chatId, message, options);
+//       const messageId = sentMessage.message_id;
 
-      setTimeout(async () => {
-        await bot.deleteMessage(chatId, messageId).catch(() => { });
-      }, timeoutMs);
-  } catch (error) {
-      console.error('Failed to send or schedule deletion for message', error);
-  }
-}
+//       setTimeout(async () => {
+//         await bot.deleteMessage(chatId, messageId).catch(() => { });
+//       }, timeoutMs);
+//   } catch (error) {
+//       console.error('Failed to send or schedule deletion for message', error);
+//   }
+// }
 
 module.exports = {
   identifyAndMarkSpammers,
